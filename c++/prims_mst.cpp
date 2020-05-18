96f57487e3086fc23dc024398630adef343ce150
shivam <shivam.dhall99@gmail.com>
2020-05-18 20:08:03 +0530

    prims mst updated

--- c++/prims_mst.cpp
+++ c++/prims_mst.cpp
@@ -1,48 +1,46 @@
 #include<iostream>
 using namespace std;
 
-int min_edge( int a[][5] ,int *t, int t1,int u, int *s, int n)
+int min_edge( int a[][5], int *s, int n)
 {
-  int p=100,min;
+  int p=1000,min;
 
   for(int i=0;i<n;i++)
-    for(int j=0;j<n;j++)
-      if(a[i][j]!=0 && s[i]==1 && s[j]!=1)
-        t[t1++]=j;
+  {
+    if(s[i]==1)
+    {
+      for(int j=0;j<n;j++)
+      {
+        if(a[i][j]!=0 && s[j]!=1 && p>a[i][j])
+        {
+          p=a[i][j];
+          min=j;
+        }
+      }
+    }
+  }
 
-  for(int j=0;j<n;j++)
-    for(int i=0;i<t1;i++)
-      if((p>a[j][t[i]]) && s[j]!=1)
-        p=a[j][t[i]], min=t[i];
-
-  return min;
+  s[min]=1;
+  return p;
 }
 
 void mst(int a[][5], int n)
 {
-  int s[n],t[n],u,t1,me,res[n],j;
+  int s[n],t[n],u,t1,me,res,j;
   for(int i=0;i<n;i++)
     s[i]=0;
 
+  res=0;
   cin>>u;
   s[u]=1;
-  res[0]=u;
 
-  for(j=1;j<=n;j++)
+  for(j=0;j<n-1;j++)
   {
-    t1=0;
-    s[u]=1;
-
-    for(int i=0;i<n;i++)
-      if(a[u][i]!=0 && s[i]!=1)
-         t[t1++]=i;
-
-    me=min_edge(a,t,t1,u,s,n);
-    res[j]=u=me;
+    me=min_edge(a,s,n);
+    res+=me;
   }
 
-  for(int i=0;i<n;i++)
-    cout<<res[i];
+    cout<<res;
 }
 
 int main()
@@ -50,11 +48,11 @@ int main()
   int n;
   cin>>n;
 
-  int a[5][5] = { { 0, 2, 0, 6, 0 },
-                  { 2, 0, 3, 0, 5 },
-                  { 0, 3, 0, 0, 7 },
-                  { 6, 0, 0, 0, 9 },
-                  { 0, 5, 7, 9, 0 } };
+  int a[5][5] = { {0,2,0,5,8 },
+                  {2,0,6,0,0 },
+                  { 0,6,0,9,0},
+                  { 5,0,9,0,10},
+                  { 8,0,0,10,0} };
 
 
   /*
